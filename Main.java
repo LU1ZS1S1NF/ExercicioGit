42de15567c41c8ee510ff888efa141db054d670c
LU1ZS1S1NF <42079550+LU1ZS1S1NF@users.noreply.github.com>
2018-08-03 21:30:28 -0300

    Adicionando "Main.java" e "Operacoes.java"

---
+++ Main.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciooperacoes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Luiz
+ */
+public class Main {
+    public static void main(String[] args){
+        Operacao operacao = new Operacao();
+        Scanner input = new Scanner(System.in);
+        
+        operacao.soma();
+        
+        double n1,n2;
+        System.out.println("Digite o primeiro numero inteiro");
+        n1 = input.nextInt();  
+        System.out.println("Digite o segundo numero inteiro");
+        n2 = input.nextInt(); 
+        operacao.calculaMedia(n1,n2);
+        
+        int resposta;
+        resposta = operacao.multiplicacao();
+        System.out.println("O produto dos numeros e " + resposta);
+        
+        double num1,num2, resposta2;
+        System.out.println("Digite o dividendo");
+        num1 = input.nextInt();  
+        System.out.println("Digite o divisor");
+        num2 = input.nextInt(); 
+        resposta2 = operacao.divisao(num1, num2);
+        System.out.println("O resultado da divisao dos numeros e " + resposta2);
+        
+        operacao.subtracao();
+        
+        double nota1, nota2;
+        System.out.println("Digite a primeira nota");
+        nota1 = input.nextInt();  
+        System.out.println("Digite a segunda nota");
+        nota2 = input.nextInt(); 
+        operacao.mediaPonderada(num1, num2);
+        
+        double fat; 
+        System.out.println("Digite o número o qual você quer o fatorial");
+        fat = input.nextInt();
+        operacao.fatorial(fat); 
+        
+        
+    }	 	  	 	     	     	      	    	      	    	        	 	
+}
